d797e3e6106294e8bb0af223152244916dad0163
Alex Karamfilov <karamfilovs@gmail.com>
2019-10-30 13:06:23 +0200

    Adding initial version of url checker

---
+++ src/main/java/HTTPClient.java
@@ -0,0 +1,74 @@
+import org.openqa.selenium.WebElement;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HTTPClient {
+    private String failedURLS = "";
+    private String succeededURLS = "";
+    private String incorrectURLS = "";
+
+
+    private boolean verifyUrl(String url) {
+        String urlRegex = "^(http|https)://[-a-zA-Z0-9+&@#/%?=~_|,!:.;]*[-a-zA-Z0-9+@#/%=&_|]";
+        Pattern pattern = Pattern.compile(urlRegex);
+        Matcher m = pattern.matcher(url);
+        if (m.matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public void validateUrl(List<String> urls) throws Exception {
+        urls.forEach((String url) -> {
+            if (verifyUrl(url)) {
+                System.out.println("Checking URL:" + url);
+                try {
+                    URL myURL = new URL(url);
+                    HttpURLConnection myConnection = (HttpURLConnection) myURL.openConnection();
+                    if (myConnection.getResponseCode() == URLStatus.HTTP_OK.getStatusCode()) {
+                        succeededURLS = succeededURLS + "\n" + url + "****** Status message is : "
+                                + URLStatus.getStatusMessageForStatusCode(myConnection.getResponseCode());
+                    } else {
+                        failedURLS = failedURLS + "\n" + url + "****** Status message is : "
+                                + URLStatus.getStatusMessageForStatusCode(myConnection.getResponseCode());
+                    }
+                } catch (Exception e) {
+                    System.out.print("For url- " + url + "" + e.getMessage());
+                }
+            } else {
+                incorrectURLS += "\n" + url;
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+        PageNavigator.startBrowser();
+        PageNavigator.gotoPage("https://pragmatic.bg");
+        List<String> pageLinks = PageNavigator.getAllPageLinks();
+        try {
+            HTTPClient myClient = new HTTPClient();
+            myClient.validateUrl(pageLinks);
+            System.out.println("Valid URLS that have successfully connected :");
+            System.out.println(myClient.succeededURLS);
+            System.out.println("\n--------------\n\n");
+            System.out.println("Broken URLS that did not successfully connect :");
+            System.out.println(myClient.failedURLS);
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+
+        PageNavigator.browserQuit();
+    }
+}
+
---
+++ src/main/java/PageNavigator.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+
+public class PageNavigator {
+    private static WebDriver driver;
+    private static final String WINDOWS_CHROME_DRIVER_PATH = "src\\main\\resources\\chromedriver.exe";
+
+
+    public static void gotoPage(String page) {
+        driver.navigate().to(page);
+    }
+
+    public static void startBrowser() {
+        System.setProperty("webdriver.chrome.driver", WINDOWS_CHROME_DRIVER_PATH);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
+    }
+
+   public static List<String> getAllPageLinks(){
+       System.out.println("Scrapping all hyperlinks from the current page");
+       List<WebElement> links = driver.findElements(By.tagName("a"));
+       links.addAll(driver.findElements(By.tagName("img")));
+       List<WebElement> pageLinks = links.stream().filter(link -> link.getAttribute("href") != null).collect(Collectors.toList());
+       List<String> hyperlinks = new ArrayList<>();
+       pageLinks.forEach(pageLink -> hyperlinks.add(pageLink.getAttribute("href")));
+       System.out.println("Links found:" + hyperlinks.size());
+       return hyperlinks;
+   }
+
+   public static void browserQuit(){
+        driver.quit();
+   }
+
+
+}
