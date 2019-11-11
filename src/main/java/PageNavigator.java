e35b19fbd1d842ea56f27dec41d3e1cce717428a
Milen Strahinski <milen.strahinski@pragmatic.bg>
2019-11-11 15:14:39 +0200

    adding a check for the cookies window

--- src/main/java/PageNavigator.java
+++ src/main/java/PageNavigator.java
@@ -3,6 +3,9 @@ import com.sun.deploy.util.SystemUtils;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.plugin2.util.SystemUtil;
@@ -30,10 +33,31 @@ public class PageNavigator {
         driver.navigate().to(page);
     }
 
+    private static boolean isPresent(By by) {
+        try {
+            driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            driver.findElement(by);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            return true;
+        } catch (NoSuchElementException e) {
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            return false;
+        }
+    }
+
     public static void acceptCookies() {
-        driver.switchTo().frame(1);
-        driver.findElement(By.xpath("//a[@class='submit']")).click();
-        driver.switchTo().parentFrame();
+        if(isPresent(By.xpath("//iframe[contains(@id,'pop-frame')]"))) {
+            System.out.println("Entering to accept the cookies.");
+            driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@id,'pop-frame')]")));
+            driver.findElement(By.cssSelector("div.pdynamicbutton a.submit")).click();
+            Wait<WebDriver> wait = new WebDriverWait(driver, 60);
+            System.out.println("Waiting for Close button to appear in order to click it.");
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".pdynamicbutton a.close"))).click();
+            System.out.println("Clicking the close button after the cookies are accepted.");
+            driver.switchTo().defaultContent();
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//iframe[contains(@id,'pop-frame')]")));
+            System.out.println("Cookies accepted!");
+        }
     }
 
     public static void takeScreenshot(String url) {
