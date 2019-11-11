20b81c1262a648a8f5fb370dd9f39d9d6b6a8722
Aleksandar Karamfilov <karamfilovs@gmail.com>
2019-11-11 09:11:55 +0200

    Adding urls for us

---
+++ src/main/java/FileReader.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileReader {
+
+    public static List<String> retrieveLinksFromFile(String filePath) {
+        List<String> links = new ArrayList<>();
+        // pass the path to the file as a parameter
+        File file =
+                new File(filePath);
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (sc.hasNextLine()) {
+            links.add(sc.next());
+
+        }
+        return links;
+    }
+}
